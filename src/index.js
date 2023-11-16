5c616a36715769060ccb1b307fe5d86e6544c830
kumawat-Ankita <145592877+kumawat-Ankita@users.noreply.github.com>
2023-11-16 22:23:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 //Write your code here
+class API {
+  #secure;
 
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = this.url.startsWith('https');
+  }
 
+  isSecure() {
+    return this.#secure;
+  }
 
-
-
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = this.url.startsWith('https');
+  }
+}
 //Do not remove this code
 module.exports = { API }
